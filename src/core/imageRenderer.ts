661c5e5452d67fde27b89109c8ba45b3db1a6366
rivmos <rivmos.official@gmail.com>
2024-12-12 00:25:40 +0530

    added zoom functionality

--- src/core/imageRenderer.ts
+++ src/core/imageRenderer.ts
@@ -1,13 +1,15 @@
 import { MediaViewer } from "./mediaViewer";
 
 export class ImageRenderer extends MediaViewer {
-    
+
     private url: string;
     private imageElement: HTMLImageElement;
+    private scale: number; // zoom state
 
     constructor(container: HTMLElement, url: string) {
         super(container)
         this.url = url;
+        this.scale = 1; // initial zoom state
 
         this.imageElement = new Image();
         this.imageElement.style.position = 'absolute';
@@ -21,10 +23,24 @@ export class ImageRenderer extends MediaViewer {
 
         this.container.appendChild(this.imageElement)
 
+        this.initEvents();
 
     }
 
+    private initEvents() {
+        this.container.addEventListener('wheel', (e) => {
+            e.preventDefault();
+            this.zoom(e.deltaY < 0 ? 0.1 : -0.1); // deltaY +ve wheel down, so for wheel down we zoom out by -0.1
+        });
+
+    }
+
+    private zoom(amount: number){
+        this.scale = Math.min(Math.max(0.5, this.scale + amount), 5); // keeping the scale between 0.5 and 5
+        this.imageElement.style.transform = `scale(${this.scale})`; // transforming image as per the current state of scale
+    }
+
     destroy(): void {
-        
+
     }
 }
