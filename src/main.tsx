7d23357f557985aeda8d53f42692491c945811d8
rivmos <rivmos.official@gmail.com>
2024-12-17 05:24:47 +0530

    flip, smooth zoom, rotate and full screen functionlity with separate css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App';
+import './index.css'
 
 // Render your React component instead
 const root = createRoot(document.getElementById('root') as HTMLElement);
