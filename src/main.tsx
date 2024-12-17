38cf6c81f7af8477f164439ad36042136d938723
rivmos <rivmos.official@gmail.com>
2024-12-17 05:47:19 +0530

    removed example files

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App';
-import './index.css'
-
-// Render your React component instead
-const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
